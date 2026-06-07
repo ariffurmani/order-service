60365774634fbd4b149f5ba7f1036aecd212a3b1
ariffurmani <furmani.af.98@gmail.com>
2026-06-07 12:18:10 +0530

    Removed unwanted files

---
+++ src/main/java/org/furmani/orderservice/exception/ProductUnavailableException.java
@@ -0,0 +1,9 @@
+package org.furmani.orderservice.exception;
+
+public class ProductUnavailableException extends RuntimeException {
+
+    public ProductUnavailableException(String message) {
+        super(message);
+    }
+}
+
